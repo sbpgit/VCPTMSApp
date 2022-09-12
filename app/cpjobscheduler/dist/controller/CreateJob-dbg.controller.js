d0550f79e9a5b6c341a6757fa0bce552554cc161
Thulja Takoor <thuljabhavanisingh@sbpcorp.in>
2022-09-12 07:42:46 +0000

    SEP1213:12

--- app/cpjobscheduler/dist/controller/CreateJob-dbg.controller.js
+++ app/cpjobscheduler/dist/controller/CreateJob-dbg.controller.js
@@ -1859,7 +1859,18 @@ sap.ui.define(
                     var buttonSel = oEvent.getSource().getText();
                     var keySel = that.byId("idJobType").getSelectedKey();
                     var IBPinteg = that.byId("idIBPselect").getSelectedKey();
+                   
+                    if(keySel === "I"){
+                        if(that.byId("idIBPselect").getSelectedKey() === "I"){
+                            var exeJobName = this.byId("idJobType").getSelectedItem().getText() + " " + that.byId("idRbtnImport").getSelectedButton().getText();
+                        } else {
 
+                            var exeJobName = this.byId("idJobType").getSelectedItem().getText() + " " + that.byId("idRbtnExport").getSelectedButton().getText();
+                        }
+                            that.oGModel.setProperty("/Jobname", exeJobName);
+                    } else {
+                        that.oGModel.setProperty("/Jobname", this.byId("idJobType").getSelectedItem().getText());
+                    }
 
                     if(buttonSel === "Schedule Job"){
                         that.oGModel.setProperty("/EcecuteType", "S");
