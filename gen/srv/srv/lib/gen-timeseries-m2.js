81e1c4c8f256981843586fea35c2f7b1556a4e55
Deepa Adla <deepaadla@sbpcorp.in>
2022-10-10 17:28:26 +0000

    2022-10-10T22:00:00

--- gen/srv/srv/lib/gen-timeseries-m2.js
+++ gen/srv/srv/lib/gen-timeseries-m2.js
@@ -56,9 +56,7 @@ class GenTimeseriesM2 {
         FROM "V_PARTIALPRODCHAR"
         WHERE "LOCATION_ID" = '` + adata.LOCATION_ID + `'
         AND ( "PRODUCT_ID" = '` + lMainProduct + `'
-        OR "REF_PRODID" = '` + lMainProduct + `' )
-        ORDER BY LOCATION_ID, 
-                PRODUCT_ID`
+        OR "REF_PRODID" = '` + lMainProduct + `' )`
         );
 
         for (let i = 0; i < liPrimaryIDMain.length; i++) {
