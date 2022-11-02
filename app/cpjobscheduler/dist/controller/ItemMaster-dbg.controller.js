3b856d4a0abb7bbf5a1d4e393781b82553bab7cc
Rajesh Ramagiri <rajeshramagiri@sbpcorp.in>
2022-11-02 11:51:30 +0000

    Nov2-17-21

--- app/cpjobscheduler/dist/controller/ItemMaster-dbg.controller.js
+++ app/cpjobscheduler/dist/controller/ItemMaster-dbg.controller.js
@@ -135,6 +135,7 @@ sap.ui.define(
           that.oList.removeSelections(true);
 
           that.getView().byId("headSearch").setValue();
+          oGModel.setProperty("/dataFlag", "");
 
         //   var nowH = new Date();
         //   //past 15 days selected date
@@ -184,6 +185,7 @@ sap.ui.define(
               // Setting the default selected item for table
             that.byId("jobList").setSelectedItem(that.byId("jobList").getItems()[0], true);
             oGModel.setProperty("/aDATA", aData[0]);
+            oGModel.setProperty("/dataFlag", "X");
               that.onhandlePress();
             },
             error: function (error) {
@@ -324,7 +326,7 @@ sap.ui.define(
                 viewName: that.oDetailView.getViewName(),
               });
             } catch (e) {
-              that.oDetailView.onAfterRendering();
+            //   that.oDetailView.onAfterRendering();
             }
           } else {
             that.bus.publish("nav", "toDetailPage", {
