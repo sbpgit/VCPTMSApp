0ad88ed192c5f8b53745a6a30830c7954ecd43fc
Pradeep Daka <pradeepkumardaka@sbpcorp.in>
2022-12-02 08:27:59 +0000

    Dec 1 13:57

--- gen/srv/srv/lib/ibpimport-service.js
+++ gen/srv/srv/lib/ibpimport-service.js
@@ -2600,45 +2600,26 @@ module.exports = cds.service.impl(async function () {
         // }
         GenF.jobSchMessage('X', lMessage, req);
     });
-    this.on("importibpversce", async (req) => {
+    this.on("importibpversce", async (request) => {
         // Get Planning area and Prefix configurations for IBP
         let liParaValue = await GenF.getIBPParameterValue();
         let flag, lMessage = '';
         let resUrl = "/" + liParaValue[0].VALUE + "?$select=VERSIONID,VERSIONNAME,SCENARIOID,SCENARIONAME&$inlinecount=allpages";
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
         let req = await service.tx(request).get(resUrl);
            
         if (req.length) {
-            await DELETE.from('CP_IBPVERSIONSCENARIO');
-        }
-
-        for (let i in req) {
-
-=======
-        console.log("RESURL" + resUrl);
->>>>>>> 006d4bf39da4635abad6b6dea96bef918e1cefca
-        let req1 = await service.tx(req).get(resUrl);
-           
-        if (req1.length) {
             // await DELETE.from('CP_IBPVERSIONSCENARIO');
             await cds.run(
                 `DELETE FROM "CP_IBPVERSIONSCENARIO" `
             );
         }
 
-        for (let i in req1) {
-<<<<<<< HEAD
-=======
-            console.log("For Loop");
->>>>>>> e71dd30691a5ccdf3ec52b2c2c2d73258c149211
->>>>>>> 006d4bf39da4635abad6b6dea96bef918e1cefca
+        for (let i in req) {
             let modQuery = 'INSERT INTO "CP_IBPVERSIONSCENARIO" VALUES (' +
-                "'" + req1[i].VERSIONID + "'" + "," +
-                "'" + req1[i].SCENARIOID + "'" + "," +
-                "'" + req1[i].VERSIONNAME + "'" + "," +
-                "'" + req1[i].SCENARIONAME + "'" + ')';
+                "'" + req[i].VERSIONID + "'" + "," +
+                "'" + req[i].SCENARIOID + "'" + "," +
+                "'" + req[i].VERSIONNAME + "'" + "," +
+                "'" + req[i].SCENARIONAME + "'" + ')';
             try {
                 await cds.run(modQuery);
                 flag = 'S';
@@ -2651,23 +2632,12 @@ module.exports = cds.service.impl(async function () {
         }
 
         if (flag === 'S') {
-<<<<<<< HEAD
             // lMessage = "Successfully imported version scenario from IBP";
             // console.log(lMessage);
             return "Success";
         } else {
             // lMessage = "Failed to import version scenario from IBP";
             // console.log(lMessage);
-=======
-            console.log("Flag S");
-            lMessage = "Successfully imported version scenario from IBP";
-            console.log(lMessage);
-            return "Success";
-        } else {
-            console.log("Flag E");
-            lMessage = "Failed to import version scenario from IBP";
-            console.log(lMessage);
->>>>>>> 006d4bf39da4635abad6b6dea96bef918e1cefca
             return "Failed";
         }
     });
